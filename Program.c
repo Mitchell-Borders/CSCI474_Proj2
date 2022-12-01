404f36aee314900c411f5ab77c2402cee8ff19d5
Mitchell-Borders <mitchell.borders28@gmail.com>
2022-12-01 17:15:09 -0600

    Working prograram

--- Program.c
+++ Program.c
@@ -3,11 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h> 
 #define SHARED 1
 sem_t empty, full;    /* the global semaphores */
 int data;             /* shared buffer         */
 int numIters;
 int total;
+
+void Producer(int calculated) {
+    sem_wait(&empty);
+    data = calculated;
+    sem_post(&full);
+}
+
+void Consumer() {
+    sem_wait(&full);
+    total += data;
+    sem_post(&empty);
+}
 
 void* CalculateFirstHalf(void* threadid) {
     FILE* inFile = fopen("data1.dat", "r");
@@ -17,6 +30,7 @@ void* CalculateFirstHalf(void* threadid) {
         fscanf(inFile, "%d\n", &lineNum);
         total += lineNum;
     }
+    printf("ACTUAL TOTAL 1: %d \n", total);
     Producer(total);
     Consumer();
     pthread_exit(NULL);
@@ -30,40 +44,32 @@ void* CalculateSecondHalf(void* threadid){
         fscanf(inFile, "%d\n", &lineNum);
         total += lineNum;
     }
+    printf("ACTUAL TOTAL 2: %d \n", total);
     Producer(total);
     Consumer();
     pthread_exit(NULL);
 }
 
+void* ServerThread(void* threadid){
 
-void Producer(int calculated) {
-    sem_wait(&empty);
-    data = calculated;
-    sem_post(&full);
-}
-
-void Consumer() {
-    sem_wait(&full);
-    total = total+data;
-    sem_post(&empty);
 }
 
 int main(int argc, char* argv[])
 {
     pthread_t pid, cid1, cid2;  
-    pthread_create(&pid, NULL, NULL, NULL);
+    pthread_create(&pid, NULL, ServerThread, NULL);
     printf("main started\n");
     sem_init(&empty, SHARED, 1);  /* sem empty = 1 */
     sem_init(&full, SHARED, 0);   /* sem full = 0  */
 
     printf("Server sends a start signal to the worker thread 1\n");
     pthread_create(&cid1, NULL, CalculateFirstHalf, NULL);
+    pthread_join(cid1, NULL);
     printf("Server receives a completion signal from Worker thread 1\n Worker thread 1 result: %d\n", data);
     printf("Server sends a start signal to the worker thread 2\n");
     pthread_create(&cid2, NULL, CalculateSecondHalf, NULL);
+    pthread_join(cid2, NULL);
     printf("Server receives a completion signal from Worker thread 1\n Worker thread 2 result: %d\n", data);
     pthread_join(pid, NULL);
-    pthread_join(cid1, NULL);
-    pthread_join(cid2, NULL)
     printf("main done\n");
 }
